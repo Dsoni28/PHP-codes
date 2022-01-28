3446211b0185504a17972270a60a7e617abc442e
Dsoni28 <98619422+Dsoni28@users.noreply.github.com>
2022-01-28 16:32:53 -0500

    Create showComment.php

---
+++ SocialMedia Functions/showComment.php
@@ -0,0 +1,20 @@
+<?php
+function showcomment($connectdata){
+
+      $sql = "SELECT * FROM comments where id='$post_id' ";
+			$result = mysqli_query($connectdata, $sql);
+			if (mysqli_num_rows($result) > 0) {
+				while ($row = mysqli_fetch_assoc($result)) {
+
+			?>
+			<div class="single-item">
+				<h4><?php echo $row['name']; ?></h4>
+				<p><?php echo $row['comment']; ?></p>
+			</div>
+			<?php
+
+				}
+			}
+
+}
+<?

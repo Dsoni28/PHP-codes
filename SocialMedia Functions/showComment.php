8ff5028e250c307303e7e3b3e2173151414a8109
Dsoni28 <98619422+Dsoni28@users.noreply.github.com>
2022-01-28 16:44:58 -0500

    Update showComment.php

--- SocialMedia Functions/showComment.php
+++ SocialMedia Functions/showComment.php
@@ -1,18 +1,18 @@
 <?php
 function showcomment($connectdata){
 
-      $sql = "SELECT * FROM *DATABASENAME* where id='$post_id' ";
-			$result = mysqli_query($connectdata, $sql);
+      $sql = "SELECT * FROM *DATABASENAME* where id='$post_id' ";  //compare post id from database
+			$result = mysqli_query($connectdata, $sql); //getting back the  result
 			if (mysqli_num_rows($result) > 0) {
 				while ($row = mysqli_fetch_assoc($result)) {
 
 			?>
-			<div class="single-item">
-				<h4><?php echo $row['name']; ?></h4>
-				<p><?php echo $row['comment']; ?></p>
-			</div>
-			<?php
-
+				//Displaying the result  
+					<div class="single-item">
+						<h4><?php echo $row['name']; ?></h4> 
+						<p><?php echo $row['comment']; ?></p>
+					</div>
+			
 				}
 			}
 

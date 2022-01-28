f3d8cab1beaf3f860b5ffdd8adde5343070860c1
Dsoni28 <98619422+Dsoni28@users.noreply.github.com>
2022-01-28 16:40:20 -0500

    Update postComment.php

--- SocialMedia Functions/postComment.php
+++ SocialMedia Functions/postComment.php
@@ -1,22 +1,16 @@
-function comment($connectdata){
+function comment($connectdata){  //get the data
    
+//when submit button get clicked..
   if (isset($_POST['submit'])) {
-      $name = $_POST['username']; 
-      $post_id = $_POST['post_id']; 
-      $comment = $_POST['comment']; 
+      $name = $_POST['username'];  //active user's name
+      $post_id = $_POST['post_id']; //id of the post for comment 
+      $comment = $_POST['comment']; //actual comment content
     
-    $sql = "INSERT INTO comments (postid, name, comment) 
-              VALUES ('$post_id', '$name', '$comment')";
+    $sql = "INSERT INTO *DATABASENAME* (postid, name, comment) 
+              VALUES ('$post_id', '$name', '$comment')";  
       
     $result = mysqli_query($connectdata, $sql);
-    if ($result) {
-      if($name=="admin"){
-            header("location: admin_page.php ");                    
-      }
-      else{
-            header("location: user_page.php ");
-      }
-    }
+    
   }
 
 }
